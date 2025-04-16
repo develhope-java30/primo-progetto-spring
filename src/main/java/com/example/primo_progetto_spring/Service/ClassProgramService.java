b1378fcdfd2f727443a4d3acebf0599246b07c57
merry <rozzimario18@gmail.com>
2025-04-16 17:14:37 +0200

    small fix: changed comments. new controller: for ClassProgram with default methods.

--- src/main/java/com/example/primo_progetto_spring/Service/ClassProgramService.java
+++ src/main/java/com/example/primo_progetto_spring/Service/ClassProgramService.java
@@ -8,27 +8,20 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-
-//Aggiungere endpoint per:
-//Creare un nuovo programma
-//Modificare un programma esistente
-//Assegnare un programma ad una classe
-//      Quest'operazione si può effettuare solo se alla classe non è ancora stato aggiunto alcuno studente.
-//      Modificare l'endpoint che aggiunge uno studente ad una classe verificando che alla classe sia già stato assegnato un corso, ritornando altrimenti un errore.
-
-
 @Service
 public class ClassProgramService {
     @Autowired ClassProgramRepository classProgramRepository;
 
-    public List<ClassProgram> allProgram(){
+    public List<ClassProgram> allPrograms(){
        return classProgramRepository.findAll();
     }
 
+    //Creare un nuovo programma
     public Optional<ClassProgram> addProgram(ClassProgram newProgram){
         return Optional.of(classProgramRepository.save(newProgram));
     }
 
+    //Modificare un programma esistente
     public Optional<ClassProgram> updateProgram(Long id, ClassProgram programToUpdate){
         if(classProgramRepository.existsById(id)){
             return Optional.empty();
@@ -41,4 +34,9 @@ public class ClassProgramService {
     public void deleteProgramById(Long id){
         classProgramRepository.deleteById(id);
     }
+
+    //Assegnare un programma ad una classe
+    //      Quest'operazione si può effettuare solo se alla classe non è ancora stato aggiunto alcuno studente.
+    //      Modificare l'endpoint che aggiunge uno studente ad una classe verificando che alla classe sia già stato assegnato un corso, ritornando altrimenti un errore.
+
 }
