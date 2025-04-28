9e384f764d98655187344a2568e0d6d3cbbec50b
Gino Santonicola <luigisantonicola1988@gmail.com>
2025-04-28 22:22:51 +0200

    add method getAllTopics pageable and sorting

--- src/main/java/com/example/primo_progetto_spring/classprogram/topic/TopicController.java
+++ src/main/java/com/example/primo_progetto_spring/classprogram/topic/TopicController.java
@@ -2,6 +2,10 @@ package com.example.primo_progetto_spring.classprogram.topic;
 
 import com.example.primo_progetto_spring.classprogram.ClassProgram;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,25 +15,26 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/topic")
 public class TopicController {
-    @Autowired TopicService topicService;
+    @Autowired
+    TopicService topicService;
 
     @GetMapping
-    public List<TopicEntity> allTopics(){
+    public List<TopicEntity> allTopics() {
         return topicService.allTopics();
     }
 
     @PostMapping
-    public ResponseEntity<TopicEntity> addTopic(@RequestBody TopicEntity newTopic){
+    public ResponseEntity<TopicEntity> addTopic(@RequestBody TopicEntity newTopic) {
         Optional<TopicEntity> addedTopic = topicService.addTopic(newTopic);
 
         return ResponseEntity.ok(addedTopic.get());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TopicEntity> updateTopic(@PathVariable Long id, @RequestBody TopicEntity topicToUpdate){
+    public ResponseEntity<TopicEntity> updateTopic(@PathVariable Long id, @RequestBody TopicEntity topicToUpdate) {
         Optional<TopicEntity> updatedTopic = topicService.updateTopic(id, topicToUpdate);
 
-        if(updatedTopic.isEmpty()){
+        if (updatedTopic.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
@@ -37,20 +42,39 @@ public class TopicController {
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> deleteTopic(Long id){
+    public ResponseEntity<Void> deleteTopic(Long id) {
         topicService.deleteTopicById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/{topicId}/program/{programId}")
-    public ResponseEntity<ClassProgram> assignTopicToProgram(@PathVariable Long topicId, @PathVariable Long programId){
+    public ResponseEntity<ClassProgram> assignTopicToProgram(@PathVariable Long topicId, @PathVariable Long programId) {
         Optional<ClassProgram> assignedTopic = topicService.assignTopicToProgram(topicId, programId);
 
-        if(assignedTopic.isEmpty()){
+        if (assignedTopic.isEmpty()) {
             ResponseEntity.badRequest().build();
         }
 
         return ResponseEntity.ok(assignedTopic.get());
+    }
+
+    @GetMapping("/pageable-sorting")
+    public ResponseEntity<List<TopicEntity>> getAllTopics(@RequestParam(required = false) String name,
+                                          @RequestParam int page,
+                                          @RequestParam int size,
+                                          @RequestParam(defaultValue = "asc") boolean ascending) {
+
+        Sort sort;
+        if (ascending) {
+            sort = Sort.by("name");
+        } else {
+            sort = Sort.by("name").descending();
+        }
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        List<TopicEntity> topics = topicService.getAllTopics(pageable);
+        return ResponseEntity.ok(topics);
     }
 
 }
--- src/main/java/com/example/primo_progetto_spring/classprogram/topic/TopicRepository.java
+++ src/main/java/com/example/primo_progetto_spring/classprogram/topic/TopicRepository.java
@@ -1,7 +1,14 @@
 package com.example.primo_progetto_spring.classprogram.topic;
 
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+import java.util.List;
+
 @Repository
-public interface TopicRepository extends JpaRepository<TopicEntity, Long> {}
+public interface TopicRepository extends JpaRepository<TopicEntity, Long> {
+//    List<TopicEntity> findAllTopics (Pageable pageable);
+}
