178f915f849b02c761ac1a6ad68d09e7f2e0d90f
Gino Santonicola <luigisantonicola1988@gmail.com>
2025-05-06 13:13:57 +0200

    fix sorted

--- src/main/java/com/example/primo_progetto_spring/util/PaginationAndSortingRequest.java
+++ src/main/java/com/example/primo_progetto_spring/util/PaginationAndSortingRequest.java
@@ -5,30 +5,34 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-
-
 public class PaginationAndSortingRequest {
     private int size;
     private int page;
     private boolean ascending;
-    private String direction;
     private String sortKey;
 
-    public PaginationAndSortingRequest(int size, int page, boolean ascending, String direction, String sortKey) {
+    public PaginationAndSortingRequest(int size, int page, boolean ascending,String sortKey) {
         this.size = size;
         this.page = page;
         this.ascending = ascending;
-        this.direction = direction;
         this.sortKey = sortKey;
     }
 
   public Pageable toPageable() {
       Sort sort;
-        if (ascending) {
-            sort = Sort.by(sortKey);
-        } else {
-            sort = Sort.by(sortKey).descending();
-        }
+
+      if (sortKey == null){
+         sort = Sort.unsorted();
+      } else {
+          if (ascending) {
+              sort = Sort.by(sortKey);
+          } else {
+              sort = Sort.by(sortKey).descending();
+          }
+      }
+
+
+
 
         return PageRequest.of(page, size, sort);
   }
